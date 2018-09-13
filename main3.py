872a90ae4c80a8446e2f1fdaed8e4b84cae1d729
Ruslan Gainanov <rgaynanov@nas.company>
2018-09-13 21:28:43 +0500

    Fixes

--- main3.py
+++ main3.py
@@ -1,17 +1,18 @@
 from bs4 import BeautifulSoup
-import pprint
+from pprint import pprint
 import requests
 
 import re
 import csv
 
-import time
+from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 user_id = 'Replika777'
 URL = 'https://baza.drom.ru/user/{}'.format(user_id)
 BASE_URL = 'https://baza.drom.ru'
+driver = webdriver.Chrome()
 
 #получение документа со скроллингом
 def get_html(url):
@@ -24,7 +25,7 @@ def get_html(url):
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
+        sleep(SCROLL_PAUSE_TIME)
         # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
@@ -63,18 +64,62 @@ def parse_user_page(content):
             "annotation": annotation,
         })
     print(type(disks), len(disks))
-    pprint.pprint(disks)
+    pprint(disks)
     return disks
+
+# content = get_disk_page('https://baza.drom.ru/chita/wheel/disc/new-enkei-66096670.html')
+# def get_disk_page(url):
+#     r = requests.get(url)
+#     return r.text.encode('cp1251')
+
+
+def get_disk_page(url):
+    driver.get(url)
+    sleep(2)
+    return driver.page_source
+
+
+def parse_disk_page(content, price):
+    soup = BeautifulSoup(content, 'html.parser')
+    diameter = soup.find('span', {'data-field': 'wheelDiameter'}).text.strip().replace(' ', '')  # Диаметр
+    disk_parameters = soup.find('div', {'data-field': 'discParameters'})
+    width = disk_parameters.find_all('span')[0].text.strip().replace(' ', '')  # Ширина
+    et = disk_parameters.find_all('span')[1].text.strip().replace(' ', '').replace('мм.', 'мм')  # Вылет (ET)
+    pcd = soup.find('span', {'data-field': 'wheelPcd'}).text.strip().replace(' ', '')  # Сверловка (PCD)
+    type = soup.find('span', {'data-field': 'diskType'}).text.strip().replace(' ', '')  # Тип
+    dia = soup.find('span', {'data-field': 'wheelPcd'}).text.strip().replace(' ', '')  # Диаметр ЦО (DIA)
+    img_block = soup.find('div', {'class': 'imagesExBig'})
+    image_links = img_block.find_all('img')
+    img = image_links[0]['src']
+    img_full = img.replace('bulletin', 'default')
+    diameter_int = int(diameter.replace('"', ''))
+    if diameter_int >= 19:
+        retail_price = price + 10000
+    else:
+        retail_price = price + 5000
+
+    return {
+        "diameter": diameter,
+        "width": width,
+        "et": et,
+        "pcd": pcd,
+        "type": type,
+        "dia": dia,
+        "retail_price": retail_price,
+        "img": img,
+        "img_full": img_full,
+    }
 
 
 # encoding='cp1251'
-def save_disks(disks, path):
-    with open(path,'w', newline='') as csvfile:
-        columns = ['title', 'link', 'price', 'annotation', 'title_short']
-        writer = csv.DictWriter(csvfile, fieldnames=columns, delimiter=',')
-        writer.writeheader()
-        # запись нескольких строк
-        writer.writerows(disks)
+def save_disks(disks, path, columns):
+    if disks:
+        with open(path,'w', newline='') as csvfile:
+            # columns = ['title', 'link', 'price', 'annotation', 'title_short']
+            writer = csv.DictWriter(csvfile, fieldnames=disks[0].keys(), delimiter=',')
+            writer.writeheader()
+            # запись нескольких строк
+            writer.writerows(disks)
         # writer.writerows(
         #     (x['title'],
         #      "{}{}".format(BASE_URL, x['link']),
@@ -82,10 +127,34 @@ def save_disks(disks, path):
         #      x['annotation'] for x in disks)
         # )
 
+def save_content(c):
+    with open("content.html", "w", encoding='utf-8') as f:
+        f.write(c)
+
+def open_content():
+    with open("content.html", "r", encoding='utf-8') as f:
+        # print(f.read())
+        return f.read()
+
+
+# driver = webdriver.Chrome()
+# driver.get("https://baza.drom.ru/chita/wheel/disc/new-enkei-66096670.html")
+# sleep(2)
+# pprint(driver.page_source)
+# driver.quit()
 
 if __name__ == '__main__':
     content = get_html(URL)
+    save_content(content)
+    # content = open_content()
     disks = parse_user_page(content)
     save_disks(disks, 'disks.csv')
+    disks_full_info = []
+    for disk in disks:
+        disk_content = get_disk_page(disk['link'])
+        add_info = parse_disk_page(disk_content, disk['price'])
+        disk.update(add_info)
+        disks_full_info.append(disk)
 
-    print("Hello")
+    save_disks_full(disks, 'disks_full.csv')
+    pprint(disks_full_info)
