496e4896912ed9fde8bb6a43ae96506585798035
Ruslan Gainanov <gromrx1@gmail.com>
2018-10-06 12:25:36 +0500

    Разделение полей содержащий несколько значений

--- finish_csv_disks.py
+++ finish_csv_disks.py
@@ -30,10 +30,47 @@ def open_full_info(path):
     return d
 
 
+def fix_diameter(d):
+    res = d.replace("\"", "").strip()
+    res = list(map(lambda x: int('0') if x == '' else int(x), res.split("/")))
+    return list(map(float, res))
+
+def fix_width(w):
+    res = w.replace("\"", "").strip()
+    res = list(map(lambda x: '0' if x == '' else x, res.split("/")))
+    return list(map(float, res))
+
+def fix_et(et):
+    res = et.replace("мм", "").strip()
+    res = list(map(lambda x: '0' if x == '' else x, res.split("/")))
+    return list(map(float, res))
+
+def fix_dia(d):
+    res = d.replace("мм.", "").strip()
+    res = res.replace(",", ".").strip()
+    res = list(map(lambda x: '0' if x == '' else x, res.split("/")))
+    return list(map(float, res))
+
+def fix_pcd(pcd):
+    res = pcd.split(',')
+    return list(map(lambda x: {"pcd": float(x.split('x')[0]),
+                               "holes": int(x.split('x')[1])},
+                res))
+
 def update(disk):
     # disk["et"]
-    return None
+    disk["diameter"] = fix_diameter(disk["diameter"])
+    disk["width"] = fix_width(disk["width"])
+    disk["et"] = fix_et(disk["et"])
+    disk["dia"] = fix_dia(disk["dia"])
+    disk["pcd"] = fix_pcd(disk["pcd"])
+
+    # return None
 
 if __name__ == '__main__':
-    disks = open_full_info('Replika777_full_info.csv')
-    pprint(disks)
+    disks = open_full_info('Replika777_full_info_10.csv')
+    new_disks = []
+    for disk in disks:
+        update(disk)
+        new_disks.append(disk)
+    pprint(new_disks)
